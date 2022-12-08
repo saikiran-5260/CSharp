80a8f7070cee576e590319db6ad02dfb1cd11a9c
saikiran-5260 <shanmukha.pithani@gmail.com>
2022-12-08 17:34:55 +0530

    sample commit

--- CollectionsExample/Collections.cs
+++ CollectionsExample/Collections.cs
@@ -1,1 +1,28 @@
 ﻿
+
+
+List<int> newList;
+newList = new List<int> { 5,7,3,1,6,10 };
+
+for(int i=0; i < newList.Count; i++)
+{
+    Console.Write(newList[i]+ " ");
+}
+Console.WriteLine("\n");
+foreach (int element in newList)
+{
+    
+    Console.Write(element + " ");
+}
+newList.Reverse();
+Console.WriteLine("\n");
+foreach (int el in newList)
+{
+    Console.Write(el+" ");
+}
+newList.Sort();
+Console.WriteLine("\n");
+foreach (int ele in newList)
+{
+    Console.Write(ele+" ");
+}
